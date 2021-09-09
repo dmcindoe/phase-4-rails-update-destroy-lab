d514d61b198ceb698b33a7a9ed1c14c21f20b140
Andrew McIndoe <amcindoe@powerhrg.com>
2021-09-09 14:11:09 -0400

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  resources :plants, only: [:index, :show, :create]
+  # resources :plants, only: [:index, :show, :create]
+  resources :plants # Update and Destroy added, all actions in play
 end
